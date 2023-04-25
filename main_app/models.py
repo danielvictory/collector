7f704b871979c9923646ee498628135cead263a5
danielvictory <danieljvictory@gmail.com>
2023-04-25 17:14:34 -0500

    add functionality: model of an event with create, display detail, and index pathways | add style through materialize css

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length = 150)
+    classifier = models.CharField(max_length = 150)
+    description = models.TextField(max_length = 300)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} is a {self.classifier}."
+    
+class Event(models.Model):
+    name = models.CharField(max_length = 150)
+    type = models.CharField(max_length = 150)
+    description = models.TextField(max_length = 300)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} is a {self.type} event."
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'event_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,43 @@
+# Import base django dependencies
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.http import HttpResponse
 
-# Create your views here.
+# Import models
+from .models import Event
+
+# Define class view(s)
+# class CollectionList(ListView):
+#     model = Item
+#     template_name = 'index.html'
+
+class EventCreate(CreateView):
+    model = Event
+    fields = '__all__'
+
+class EventDetail(DetailView):
+    model = Event
+
+
+# Define function view(s)
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1><a href="events/">Index</a>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def events_index(request):
+    events = Event.objects.all()
+    return render(
+        request,
+        'events/index.html',
+        {'events':events}
+    )
+
+def events_detail(request, event_id):
+    event = Event.objects.get(id=event_id)
+    return render(
+        request,
+        'events/detail.html',
+        {'event':event,}
+    )
